db6a62132a204780e57c132c32c5f697b2f6619b
Chris Archibald <carchi8py@gmail.com>
2016-01-09 15:02:06 -0800

    Completed Search controller and UI.

---
+++ Contacts/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  Contacts
+//
+//  Created by Chris Archibald on 1/9/16.
+//  Copyright © 2016 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
